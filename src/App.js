ce66c1ca6dfdd556cb91e52e4b7f8e1f7c4751b6
Rohan <rohanvrm02@gmail.com>
2021-03-10 20:20:51 +0530

    used class

--- src/App.js
+++ src/App.js
@@ -29,6 +29,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App()
-{}
+class App extends React.Component
+{
+    
+  
+  render() {
+  return (
+    <div className="App" >
+      <h1 className="App-header">
+          Hello, world!
+        <img src={logo} alt="description" className="App-logo"/> 
+        
+        <div >
+          <img  className= "logo" alt="" src={logo} width="100" height="100" />
+          <h2 className="App-title">
+            <br/>
+            TO DO  
+          </h2>
+          
+          
+        <input type="text" className="input-text" placeholder="Write a todo"/>
+       
+        </div>
+        <p>First time implementation</p>
+
+        <div>
+          <button className="add-button" > Submit</button>
+
+          <ul>
+            <li>
+              <input type="checkbox" className=""/>
+              Record Youtube video
+              <button className="btn"> Delete</button>
+            </li>
+          </ul>
+
+        </div>
+        
+        
+      </h1>
+     
+    </div>
+  );
+}
+}
+
+export default App;
 
